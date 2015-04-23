6ff9701517940c08e33d06f4ac9a5537bfefc6eb
Lars Magne Ingebrigtsen <larsi@gnus.org>
2015-04-23 18:44:47 +0200

    Initial version

--- git-stats.el
+++ git-stats.el
@@ -24,6 +24,46 @@
 
 ;;; Code:
 
+(defun git-stats (dir)
+  "Tally up commits per author per month in DIR."
+  (interactive "DGit directory to get logs for: ")
+  (let ((default-directory dir)
+	(case-fold-search t)
+	(prev-month -1)
+	authors totals)
+    (with-temp-buffer
+      (call-process "git" nil t nil "log")
+      (goto-char (point-min))
+      (while (re-search-forward "^commit " nil t)
+	(forward-line 1)
+	(let (author date)
+	  (while (not (eq (following-char) ?\n))
+	    (cond
+	     ((looking-at "author:[ \t]+\\(.*\\)")
+	      (setq author (match-string 1)))
+	     ((looking-at "date:[ \t]+\\(.*\\)")
+	      (setq date (match-string 1))))
+	    (forward-line 1))
+	  (when (and author date)
+	    (setq author (car (mail-header-parse-address author))
+		  date (parse-time-string date))
+	    (when (or (not (= prev-month (nth 4 date)))
+		      (not (save-excursion
+			     (search-forward "\ncommit " nil t))))
+	      (when authors
+		(push (cons (format-time-string "%Y-%d-01"
+						(apply 'encode-time date))
+			    (hash-table-count authors))
+		      totals))
+	      (setq authors (make-hash-table :test 'equal)
+		    prev-month (nth 4 date)))
+	    (incf (gethash author authors 0))))))
+    (pop-to-buffer "*totals*")
+    (erase-buffer)
+    (dolist (elem (nreverse totals))
+      (insert (format "%s %d\n" (car elem) (cdr elem))))))
+		      
+
 (provide 'git-stats)
 
 ;;; git-stats.el ends here
